948eddfd4fc5a210fc054db0bb0da3990f565adf
mohitchhiller <mkjangid129@gmail.com>
2021-06-16 15:18:07 +0530

    response changed

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-"use strict";
-const newGame = document.querySelector(".newGame");
-const rollDice = document.querySelector(".rollDice");
-const hold = document.querySelector(".hold");
-const player1 = document.querySelector(".player1");
-const player2 = document.querySelector(".player2");
+'use strict';
+const newGame = document.querySelector('.newGame');
+const rollDice = document.querySelector('.rollDice');
+const hold = document.querySelector('.hold');
+const player1 = document.querySelector('.player1');
+const player2 = document.querySelector('.player2');
 
-const dice = document.querySelector(".dice");
-let score1 = document.querySelector(".current-score1");
-let score2 = document.querySelector(".current-score2");
+const dice = document.querySelector('.dice');
+let score1 = document.querySelector('.current-score1');
+let score2 = document.querySelector('.current-score2');
 
-const playerActive = document.querySelector(".player-active");
+const playerActive = document.querySelector('.player-active');
 let currentScore = 0;
 let playing = true;
 let activePlayer = 1;
@@ -19,15 +19,15 @@ const switchPlayer = function () {
   currentScore = 0;
   document.querySelector(`.current-score${activePlayer}`).textContent = 0;
   activePlayer = activePlayer === 1 ? 2 : 1;
-  player1.classList.toggle("player-active");
-  player2.classList.toggle("player-active");
+  player1.classList.toggle('player-active');
+  player2.classList.toggle('player-active');
 };
 
-rollDice.addEventListener("click", function () {
+rollDice.addEventListener('click', function () {
   if (playing) {
     const diceNumber = Math.trunc(Math.random() * 6 + 1);
     dice.src = `img/dice-${diceNumber}.png`;
-    dice.classList.remove("hidden");
+    dice.classList.remove('hidden');
     if (diceNumber !== 1) {
       currentScore += diceNumber;
       document.querySelector(`.current-score${activePlayer}`).textContent =
@@ -38,20 +38,20 @@ rollDice.addEventListener("click", function () {
   }
 });
 
-hold.addEventListener("click", function () {
+hold.addEventListener('click', function () {
   if (playing) {
     scores[activePlayer - 1] += currentScore;
     document.querySelector(`.total-score${activePlayer}`).textContent =
       scores[activePlayer - 1];
     currentScore = 0;
 
-    if (scores[activePlayer - 1] >= 100) {
+    if (scores[activePlayer - 1] > 10) {
       document
         .querySelector(`.player${activePlayer}`)
-        .classList.add("player-win");
+        .classList.add('player-win');
       document
         .querySelector(`.player${activePlayer}`)
-        .classList.remove("player-active");
+        .classList.remove('player-active');
       document.querySelector(`.current-score${activePlayer}`).textContent = 0;
       playing = false;
     } else {
@@ -60,10 +60,10 @@ hold.addEventListener("click", function () {
   }
 });
 
-newGame.addEventListener("click", function () {
+newGame.addEventListener('click', function () {
   document
     .querySelector(`.player${activePlayer}`)
-    .classList.remove("player-win");
+    .classList.remove('player-win');
   currentScore = 0;
   scores = [0, 0];
   score1.textContent = 0;
@@ -71,8 +71,8 @@ newGame.addEventListener("click", function () {
   if (activePlayer === 2) {
     switchPlayer();
   }
-  document.querySelector(".total-score1").textContent = 0;
-  document.querySelector(".total-score2").textContent = 0;
+  document.querySelector('.total-score1').textContent = 0;
+  document.querySelector('.total-score2').textContent = 0;
   playing = true;
-  dice.classList.add("hidden");
+  dice.classList.add('hidden');
 });
